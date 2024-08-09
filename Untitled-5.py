ed23d5b606b70b37fddb479244c2af96266d7d5e
HoLon118 <91692762+HoLon118@users.noreply.github.com>
2024-08-09 17:29:47 +0800

    New content
    a code for Instagram new post of Python Class paragraph

---
+++ Untitled-5.py
@@ -0,0 +1,12 @@
+# class
+class Cat():
+    def __init__(self, weight, age):
+        self.weight = weight
+        self.age = age
+        self.leg = 4
+
+年糕 = Cat(7.5, 3)
+print(年糕.weight)
+print(年糕.age)
+print(年糕.leg)
+
---
+++ Untitled-6.py
@@ -0,0 +1,9 @@
+# class
+class Beverage():
+  def __init__(self, brand, size):
+    self.name = brand
+    self.size = size
+
+a = Beverage("15籃", "XL")
+print(a.name)
+print(a.size)
