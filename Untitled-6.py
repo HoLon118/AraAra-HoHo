bf246339cf7b2df3786530ac3d9d14f370c583ca
HoLon118 <91692762+HoLon118@users.noreply.github.com>
2024-08-09 21:31:24 +0800

    Class lesson 3
    how to create classes and setting method of estimate profit.

--- Untitled-6.py
+++ Untitled-6.py
@@ -1,9 +1,8 @@
-# class
-class Beverage():
-  def __init__(self, brand, size):
-    self.name = brand
-    self.size = size
+class Cat():
+    def __init__(self, weight):
+        self.weight = weight
+    def meow(self):
+        print("meow" + "~" * round(self.weight))
 
-a = Beverage("15籃", "XL")
-print(a.name)
-print(a.size)
+年糕 = Cat(7.5)
+年糕.meow()
---
+++ Untitled-7.py
@@ -0,0 +1,52 @@
+class Vegetable():
+    def __init__(self, weight, max_num, profit):
+        self.weight = weight
+        self.max_num = max_num
+        self.profit = profit
+        self.load = 0
+
+class Load():
+    def __init__(self, goods, max_load):
+        self.goods = goods
+        self.max_load = max_load
+            
+    def estimate_profit(self):
+        load = 0
+        p = 0
+        for good in self.goods:
+            load += good.weight
+            if (load > self.max_load):
+                print("it is over load.")
+                return
+            p += good.profit
+        return p
+        
+    def increase_load(self):
+        self.goods[0].load += 1
+        for index, good in enumerate(self.goods):
+            if(good.load > good.max_num):
+                good.load = 0
+                if(index < len(self.goods) - 1):
+                    self.goods[index + 1] += 1
+                else:
+                    good.load = good.max_num
+                    print("All goods are achieved max num")
+
+apples = Vegetable(4, 11, 6)
+bananas = Vegetable(5, 30, 10)
+coconuts = Vegetable(7, 4, 10)
+durian = Vegetable(15, 8, 23)
+eggplants = Vegetable(3, 22, 5)
+list_of_goods = [
+    apples,
+    bananas,
+    coconuts,
+    durian,
+    eggplants
+]
+for good in list_of_goods:
+    good.load = 1
+car = Load(list_of_goods, 50)
+
+profit = car.estimate_profit()
+print(profit)
