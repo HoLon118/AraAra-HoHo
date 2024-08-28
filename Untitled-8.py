687e2b1e1dd9927392a628271f6aefbef8470034
HoLon118 <91692762+HoLon118@users.noreply.github.com>
2024-08-29 00:38:57 +0800

    庭院深深深幾許
    fix: optimize program. delete none necessary print

--- Untitled-8.py
+++ Untitled-8.py
@@ -5,20 +5,17 @@ class StoreTemp():
         self.full = 0
     def add(self):
         """
-        make first digit +1.
+        make first digit += 1.
         then do carry (if need to)
         """
         self.nums[0] += 1
-        for i in range(self.length):
-            if (self.nums[i] == self.length and 
-                i != self.length - 1):
+        for i in range(self.length - 1):
+            if (self.nums[i] == self.length):
                 self.nums[i] = 0
                 self.nums[i+1] += 1
-            else:
-                if (i == self.length - 1 and
-                    self.nums[i] == self.length):
-                    self.nums = [0] * length
-                    self.full = 1
+        if (self.nums[-1] == self.length):
+            self.nums = [0] * length
+            self.full = 1
     def check(self):
         """
         if there is no repeat element.
@@ -30,27 +27,21 @@ class StoreTemp():
                 return 1
             else:
                 t.append(str(i))
-        print("no repeat")
         return 0
 
     
 stringArray = ["庭","院","深","深","深","幾","許"]
 length = len(stringArray)
 temp = StoreTemp(length)
-print("length:", length)
-print("temp:", temp.nums)
 store = []
 
 while(temp.full != 1):
-    print(temp.nums)
     t = []
     if temp.check() == 0:
         for i in temp.nums:
             t.append(stringArray[i])
         if t not in store:
             store.append(t)
-            print("store:", t)
-
     temp.add()
 
 for i, s in enumerate(store): 
